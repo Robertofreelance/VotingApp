c2c6233e51a0e042e343d2e72bfa378878dda3ed
Roberto <robertoborjasfreelance@gmail.com>
2021-03-06 13:31:47 -0500

    launched

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://Bempercibal22:Bempercibal22@cluster0.70cwz.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
